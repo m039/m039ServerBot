1bafb448faa5cb374758d45a5cf1b54b39ce57ec
Dmitry Mozgin <m0391n@gmail.com>
2024-02-07 23:23:05 +0300

    Update Server.php

--- src/Server.php
+++ src/Server.php
@@ -10,21 +10,46 @@ $count = 0;
 $runnginMode = new PollingPlus();
 $timerMessageId = null;
 $timerChatId = null;
+$remindTimer = null;
+$remindChatId = null;
 
 $bot->setRunningMode($runnginMode);
+
+$bot->middleware(function (Nutgram $bot, $next) {
+    if ($bot->userId() == 74792267) {
+        $next($bot);
+    } else {
+        $bot->sendMessage("Access is denied.");
+    }
+});
 
 $runnginMode->onUpdate(function (Nutgram $bot, int $delta) {
     global $timerMessageId;
     global $timerChatId;
+    global $remindChatId;
+    global $remindTimer;
     
     if ($timerMessageId && $timerChatId) {
         $bot->editMessageText("Timer: " . date("F j, Y, g:i:s a") . "\n", $timerChatId, $timerMessageId);
     }
+
+    if (is_numeric($remindTimer)) {
+        $remindTimer -= $delta;
+
+        if ($remindTimer < 0) {
+            $bot->sendMessage("You got a reminder.", $remindChatId);
+            $remindTimer = $remindChatId = null;
+        }
+    }
 });
 
 $bot->onCommand('start', function(Nutgram $bot) {
+    $bot->sendMessage('Ciao!');
+});
+
+$bot->onCommand("clicker", function (Nutgram $bot) {
     global $count;
-    $bot->sendMessage('Ciao! ' . $count);
+    $bot->sendMessage('Click: ' . $count);
     $count++;
 });
 
@@ -35,6 +60,15 @@ $bot->onCommand("start_timer", function(Nutgram $bot) {
     $message = $bot->sendMessage("Timer: ");
     $timerMessageId = $message->message_id;
     $timerChatId = $message->chat->id;
+});
+
+$bot->onCommand("remind", function (Nutgram $bot) {
+    global $remindChatId;
+    global $remindTimer;
+
+    $remindTimer = 60;
+    $remindChatId = $bot->chatId();
+    $bot->sendMessage("The notification is set.");
 });
 
 $bot->onCommand("stop_timer", function (Nutgram $bot) {
