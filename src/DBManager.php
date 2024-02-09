e6850758d70951bfaa648d747ec20be0beaf5a99
Dmitry Mozgin <m0391n@gmail.com>
2024-02-09 14:03:52 +0300

    Fix a big

--- src/DBManager.php
+++ src/DBManager.php
@@ -10,6 +10,8 @@ class DBManager {
     private string $database;
 
     private $connection;
+
+    private $previousTime;
     
     public function __construct(
         string $host = null, 
@@ -21,14 +23,22 @@ class DBManager {
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
+        
+        $this->createTableIfNotExist();
+    }
 
-        try {
-            $this->connection = new \PDO("mysql:host=$this->host;dbname=$this->database", $this->username, $this->password);
-        } catch (\PDOException $e) {
-            die ("Can't connect to database: " . $e->getMessage());
+    private function getPDO() : \PDO {
+        $time = time();
+        if (!$this->previousTime || $this->previousTime + 60 < $time) {
+            try {
+                $this->connection = new \PDO("mysql:host=$this->host;dbname=$this->database", $this->username, $this->password);
+            } catch (\PDOException $e) {
+                die ("Can't connect to database: " . $e->getMessage());
+            }
+            $this->previousTime = $time;
         }
 
-        $this->createTableIfNotExist();
+        return $this->connection;
     }
 
     static public function createInstance() : DBManager {
@@ -41,24 +51,24 @@ class DBManager {
     }
 
     public function getAllEntries() : array {
-        $statement = $this->connection->query("SELECT * FROM server_bot");
+        $statement = $this->getPDO()->query("SELECT * FROM server_bot");
         return $statement->fetchAll(\PDO::FETCH_CLASS, DBEntry::class);
     }
 
     public function register(int $userId, int $chatId) {
-        $statement = $this->connection->prepare("SELECT _id FROM server_bot WHERE user_id=? AND chat_id=? LIMIT 1");
+        $statement = $this->getPDO()->prepare("SELECT _id FROM server_bot WHERE user_id=? AND chat_id=? LIMIT 1");
         $statement->bindParam(1, $userId, \PDO::PARAM_INT);
         $statement->bindParam(2, $chatId, \PDO::PARAM_INT);
         $statement->execute();
         $row = $statement->fetch(\PDO::FETCH_ASSOC);
         if (!$row) {
             echo "Registering a new subscriber.\n";
-            $statement = $this->connection->prepare("INSERT INTO server_bot (user_id, chat_id) VALUES (?, ?)");
+            $statement = $this->getPDO()->prepare("INSERT INTO server_bot (user_id, chat_id) VALUES (?, ?)");
             $statement->bindParam(1, $userId, \PDO::PARAM_INT);
             $statement->bindParam(2, $chatId, \PDO::PARAM_INT);
             $statement->execute();
         } else {
-            $statement = $this->connection->prepare("UPDATE server_bot SET subscribed=1 WHERE user_id=? AND chat_id=?");
+            $statement = $this->getPDO()->prepare("UPDATE server_bot SET subscribed=1 WHERE user_id=? AND chat_id=?");
             $statement->bindParam(1, $userId, \PDO::PARAM_INT);
             $statement->bindParam(2, $chatId, \PDO::PARAM_INT);
             $statement->execute();
@@ -66,7 +76,7 @@ class DBManager {
     }
 
     public function unregister(int $userId, int $chatId) {
-        $statement = $this->connection->prepare("UPDATE server_bot SET subscribed=0 WHERE user_id=? AND chat_id=?");
+        $statement = $this->getPDO()->prepare("UPDATE server_bot SET subscribed=0 WHERE user_id=? AND chat_id=?");
         $statement->bindParam(1, $userId, \PDO::PARAM_INT);
         $statement->bindParam(2, $chatId, \PDO::PARAM_INT);
         $statement->execute();
@@ -76,7 +86,7 @@ class DBManager {
         $server_is_online = $entry->server_is_online? 1: 0;
         $subscribed = $entry->subscribed? 1 : 0;
 
-        $statement = $this->connection->prepare("UPDATE server_bot SET server_is_online=?, subscribed=? WHERE _id=?");
+        $statement = $this->getPDO()->prepare("UPDATE server_bot SET server_is_online=?, subscribed=? WHERE _id=?");
         $statement->bindParam(1, $server_is_online, \PDO::PARAM_INT);
         $statement->bindParam(2, $subscribed, \PDO::PARAM_INT);
         $statement->bindParam(3, $entry->_id, \PDO::PARAM_INT);
@@ -85,10 +95,10 @@ class DBManager {
 
     private function createTableIfNotExist() {
         try {
-            $this->connection->query("DESCRIBE server_bot");
+            $this->getPDO()->query("DESCRIBE server_bot");
         } catch (PDOException $e) {
             echo "Creating the table.\n";
-            $this->connection->query(
+            $this->getPDO()->query(
                 "CREATE TABLE server_bot (_id INT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, chat_id INT NOT NULL, server_is_online BOOLEAN NOT NULL DEFAULT 1, subscribed BOOLEAN NOT NULL DEFAULT 1, PRIMARY KEY (_id))"
             );
         }
