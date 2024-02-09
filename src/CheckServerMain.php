e9a6086b04e24c0b1c7f2f6c11c682fb6b041ad6
Dmitry Mozgin <m0391n@gmail.com>
2024-02-10 02:43:37 +0300

    Add access check

--- src/CheckServerMain.php
+++ src/CheckServerMain.php
@@ -7,6 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use m039\ServerChecker;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Configuration;
+use SergiX44\Nutgram\Telegram\Exceptions\TelegramException;
 
 $db = DBManager::createInstance();
 $entries = $db->getAllEntries();
@@ -36,10 +37,16 @@ foreach ($subscribed as $subscriber) {
         continue;
     }
 
-    if ($server_is_online) {
-        $bot->sendMessage("Сервер в сети", $subscriber->chat_id);
-    } else {
-        $bot->sendMessage("Не удается достучаться до сервера", $subscriber->chat_id);
+    try {
+        if ($server_is_online) {
+            $bot->sendMessage("Сервер в сети", $subscriber->chat_id);
+        } else {
+            $bot->sendMessage("Не удается достучаться до сервера", $subscriber->chat_id);
+        }
+    } catch (TelegramException $e) {
+        if ($e->getCode() == 403) {
+            $subscriber->subscribed = false;
+        }
     }
 
     $subscriber->server_is_online = $server_is_online;
